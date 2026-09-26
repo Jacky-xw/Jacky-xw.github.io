4d7ea332b6f357120d697be920e44347f7702724
Jacky_xw <zxw13145157@gmail.com>
2026-09-26 20:12:12 +0800

    Delete app directory

--- app/src/main/java/com/ruru/practice/RuruApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.ruru.practice
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class RuruApplication : Application()
--- app/src/main/java/com/ruru/practice/feature/home/HomeState.kt
+++
@@ -1,6 +0,0 @@
-package com.ruru.practice.feature.home
-
-data class HomeState(
-    val focus: String = "安般念",
-    val reminder: String = "保持觉知"
-)
