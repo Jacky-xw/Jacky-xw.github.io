747e83b23b0e9b7f861476775cb50b0e79e5e286
Jacky_xw <zxw13145157@gmail.com>
2026-09-26 18:23:50 +0800

    Delete app directory

--- app/src/main/java/com/ruru/practice/feature/meditation/MeditationHistoryState.kt
+++
@@ -1,6 +0,0 @@
-package com.ruru.practice.feature.meditation
-
-data class MeditationHistoryState(
-    val totalMinutes: Int = 0,
-    val sessionCount: Int = 0
-)
--- app/src/main/java/com/ruru/practice/feature/meditation/MeditationResultState.kt
+++
@@ -1,7 +0,0 @@
-package com.ruru.practice.feature.meditation
-
-data class MeditationResultState(
-    val durationMinutes:Int = 0,
-    val completed:Boolean = false,
-    val observation:String = ""
-)
