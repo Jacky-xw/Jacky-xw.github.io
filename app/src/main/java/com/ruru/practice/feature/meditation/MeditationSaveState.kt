01eb017f8c621688ff6448b49b298b20e3ec538b
Jacky_xw <zxw13145157@gmail.com>
2026-09-26 19:48:01 +0800

    Delete app directory

--- app/src/main/java/com/ruru/practice/feature/meditation/MeditationSaveState.kt
+++
@@ -1,8 +0,0 @@
-package com.ruru.practice.feature.meditation
-
-data class MeditationSaveState(
-    val isSaving: Boolean = false,
-    val saved: Boolean = false,
-    val message: String? = null,
-    val error: String? = null
-)
