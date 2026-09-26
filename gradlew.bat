0042259b2423f8b0407a1e9e5e3a8c604b0c013a
Jacky_xw <zxw13145157@gmail.com>
2026-09-26 21:52:16 +0800

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-where gradle >nul 2>nul
-if %ERRORLEVEL% EQU 0 (
-  gradle %*
-  exit /b %ERRORLEVEL%
-)
-echo 未检测到本机 Gradle。请安装 Gradle 8.11.1 后重新运行，或在 Android Studio 中打开此项目。
-exit /b 1
